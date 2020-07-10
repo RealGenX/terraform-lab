55a756a4a21ddd29665b1058070099808b1d7bfb
A Shalobalo <379064+shalobalo@users.noreply.github.com>
2020-07-09 20:55:50 -0700

    Update 01_provider.tf

--- 01_provider.tf
+++ 01_provider.tf
@@ -7,10 +7,10 @@ provider "aws" {
   secret_key = var.secret_key
   region     = var.region
 }
-terraform {
-  backend "s3" {
-    bucket = "terraform-lab-07092020"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-lab-07092020"
+#     key    = "state/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
